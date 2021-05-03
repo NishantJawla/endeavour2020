bf0c1035a6e4aa22e00ed6f8835a83d85eafb2db
Nishant Jawla <40868641+NishantJawla@users.noreply.github.com>
2021-05-03 21:29:52 +0530

    Revert "schema updated"
    This reverts commit ebff5d026979b037c32dfae926a79a578db57d3b.

--- routes/admin.js
+++ routes/admin.js
@@ -1,4 +1,3 @@
-//jshint esversion: 8
 const express = require('express');
 const route = express.Router();
 var {
--- server/modles/event.js
+++ server/modles/event.js
@@ -30,47 +30,47 @@ eventSchema.statics.FindAllEvent = function () {
 };
 
 var Event = mongoose.model("Event", eventSchema);
-Event.create({
-   "eventName": "Hackathon",
-   "category": "team",
-   "maxMember": 4,
-   "maxRound":1
-}, {
-   "eventName": "B-Plan",
-   "category": "team",
-   "maxMember": 3,
-   "maxRound": 1
-}, {
-   "eventName": "B-Quiz",
-   "category": "team",
-   "maxMember": 2,
-   "maxRound": 3
-}, {
-  "eventName": "Co-Chess",
-   "category": "team",
-   "maxMember": 2,
-   "maxRound": 3
-}, {
-   "eventName": "Market Watch",
-   "category": "team",
-   "maxMember": 2,
-   "maxRound": 1
-}, {
-   "eventName": "Memethon",
-   "category": "team",
+//Event.create({
+ //   "eventName": "Hackathon",
+ //   "category": "team",
+ //   "maxMember": 4,
+ //   "maxRound":1
+//}, {
+ //   "eventName": "B-Plan",
+ //   "category": "team",
+ //   "maxMember": 3,
+ //   "maxRound": 1
+//}, {
+ //   "eventName": "B-Quiz",
+ //   "category": "team",
  //   "maxMember": 2,
-   "maxRound": 2
-},{
-   "eventName": "Your Story_Video",
-   "category": "team",
-   "maxMember": 2,
-   "maxRound": 1
-}, {
-   "eventName": "Fantasy Premier League",
-   "category": "team",
-   "maxMember": 2,
-   "maxRound": 1
-});
+ //   "maxRound": 3
+//}, {
+//   "eventName": "Co-Chess",
+//    "category": "team",
+//    "maxMember": 2,
+//    "maxRound": 3
+//}, {
+//    "eventName": "Market Watch",
+//    "category": "team",
+//    "maxMember": 2,
+//    "maxRound": 1
+//}, {
+ //   "eventName": "Memethon",
+ //   "category": "team",
+ ////   "maxMember": 2,
+ //   "maxRound": 2
+//},{
+//    "eventName": "Your Story_Video",
+//    "category": "team",
+//    "maxMember": 2,
+//    "maxRound": 1
+//}, {
+  //  "eventName": "Fantasy Premier League",
+ //   "category": "team",
+ //   "maxMember": 2,
+ //   "maxRound": 1
+//});
 
 module.exports = {
     Event
